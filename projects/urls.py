7d31e896f84d3f36e76e57465b1131bdf1ace29d
Nicolas Keil <nicolaskeil.school@gmail.com>
2021-01-18 18:22:34 -0800

    individual pages for each project

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'projects'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:project_id>/', views.details, name='details'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Project
 
 def index(request):
-    return render(request, 'projects/index.html', {})
+    projects = Project.objects.all()
+    return render(request, 'projects/index.html', {'projects' : projects}) 
+
+def details(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    return render(request, 'projects/details.html', { 'project' : project })
