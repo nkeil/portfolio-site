c52aa0964399f058eabc3277fb080e6ed7ed9307
Nicolas Keil <nicolaskeil.school@gmail.com>
2021-01-18 18:23:55 -0800

    debug should be false

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'l#=av-#73-qg^(39@18-nfkw=lue^whhj)4kl76q8(pdz$ld49'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.nickkeil.com', '127.0.0.1']
 
